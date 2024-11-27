e45b88695e71e1287546e551dbb0fdac48a037b7
Javidev <javiernu16@gmail.com>
2024-11-26 20:59:01 -0500

    Change the random roll dice function to use the array faces

--- main.js
+++ main.js
@@ -1,100 +1,51 @@
 const $ = (el) => document.querySelector(el);
 const $$ = (els) => document.querySelectorAll(els);
 
-faces = ["1", "2", "3", "4", "5", "6"];
+const audioRoll = $("#rollDiceAudio");
 
 const body = $("body");
+const btnRoll = $("#btnRollDice");
+
 const faceDice1 = $(".faceDice1");
-const faceDice21 = $(".faceDice2");
+const faceDice2 = $(".faceDice2");
 
 const dice1 = $(".dice1");
 const dice2 = $(".dice2");
 
-const audioRoll = $("#rollDiceAudio");
-
 audioRoll.preload = "auto";
+
+const faces = ["1", "2", "3", "4", "5", "6"];
 
 body.addEventListener("keypress", (e) => {
   let code = e.key;
   if (code === " ") {
     audioRoll.currentTime = 0;
     audioRoll.play();
-
     setTimeout(rollDice(), 500);
   }
   console.log(code);
 });
 
 btnRoll.addEventListener("click", () => {
-  audioRoll.currentTime = 0;
-  audioRoll.play();
+  // audioRoll.currentTime = 0;
+  // audioRoll.play();
+  // setTimeout(rollDice(faces), 500);
+  // btnRoll.blur();
 
-  setTimeout(rollDice(), 500);
-  btnRoll.blur();
+  rollDice(faces);
 });
 
 const rollDice = () => {
   dice1.classList.toggle("animation1");
   dice2.classList.toggle("animation2");
+
+  randomRollDice(faces);
+
+  // const randomNum1 = faces[Math.floor(Math.random() * faces.length)];
+  // const randomNum2 = faces[Math.floor(Math.random() * faces.length)];
+  // console.log(randomNum1, randomNum2);
   // initial.classList.remove("initial");
-
-  front1.classList.remove("visible");
-  back1.classList.remove("visible");
-  left1.classList.remove("visible");
-  right1.classList.remove("visible");
-  bottom1.classList.remove("visible");
-  top1.classList.remove("visible");
-
-  front2.classList.remove("visible");
-  back2.classList.remove("visible");
-  left2.classList.remove("visible");
-  right2.classList.remove("visible");
-  bottom2.classList.remove("visible");
-  top2.classList.remove("visible");
-
-  const { randomNum1, randomNum2 } = randomRollDice();
-
-  switch (randomNum1) {
-    case 0:
-      front1.classList.add("visible");
-      break;
-    case 1:
-      back1.classList.add("visible");
-      break;
-    case 2:
-      left1.classList.add("visible");
-      break;
-    case 3:
-      right1.classList.add("visible");
-      break;
-    case 4:
-      bottom1.classList.add("visible");
-      break;
-    case 5:
-      top1.classList.add("visible");
-      break;
-  }
-
-  switch (randomNum2) {
-    case 0:
-      front2.classList.add("visible");
-      break;
-    case 1:
-      back2.classList.add("visible");
-      break;
-    case 2:
-      left2.classList.add("visible");
-      break;
-    case 3:
-      right2.classList.add("visible");
-      break;
-    case 4:
-      bottom2.classList.add("visible");
-      break;
-    case 5:
-      top2.classList.add("visible");
-      break;
-  }
+  // const { randomNum1, randomNum2 } = randomRollDice();a
 
   setTimeout(function () {
     dice1.classList.toggle("animation1");
@@ -102,8 +53,9 @@ const rollDice = () => {
   }, 500);
 };
 
-const randomRollDice = () => {
-  const randomNum1 = Math.floor(Math.random() * 6);
-  const randomNum2 = Math.floor(Math.random() * 6);
+const randomRollDice = (faces) => {
+  const randomNum1 = faces[Math.floor(Math.random() * faces.length)];
+  const randomNum2 = faces[Math.floor(Math.random() * faces.length)];
+  console.log(randomNum1, randomNum2);
   return { randomNum1, randomNum2 };
 };
